f15543f2fa98825630f471f23548bb5d29545ab7
Tristan <tristan.todie@gmail.com>
2022-03-10 18:15:05 +0100

    scuffed ending

--- index.js
+++ index.js
@@ -5,36 +5,72 @@ window.onload = () => {
     const ctx = canvas.getContext('2d')
     ctx.imageSmoothingEnabled = false
     ctx.font = '16px Aerial'
-    //ctx.textAlign = 'center' //ça éclate le fillMixedText 
-
+    const spellCast = new CustomEvent('spellcast')
+    const wordCast = new CustomEvent('wordcast')
+    const startGame = new CustomEvent('startgame')
+    const endGame = new CustomEvent('endgame')
+    const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor, quam id finibus euismod, purus quam luctus magna, convallis sollicitudin velit erat id arcu.'
     let incantation = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor, quam id finibus euismod, purus quam luctus magna, convallis sollicitudin velit erat id arcu.'
     incantation = incantation.replaceAll(',','')
     incantation = incantation.replaceAll('.','')
     incantation = incantation.split(' ')
+    let gameStarted = false
+
+    class Sprite{
+        constructor(){
+            this.numColumns = 4
+            this.frameWidth = 32
+            this.frameHeight = 32
+            this.currentFrame = 0 
+            this.avatarDown = new Image()
+            this.avatarDown.onload = () => {}
+            this.avatarDown.src = 'Character_Down.png'
+            this.avatarUp = new Image()
+            this.avatarUp.onload = () => {}
+            this.avatarUp.src = 'Character_Up.png'
+            this.avatarLeft = new Image()
+            this.avatarLeft.onload = () => {}
+            this.avatarLeft.src = 'Character_Left.png'
+            this.avatarRight = new Image()
+            this.avatarRight.onload = () => {}
+            this.avatarRight.src = 'Character_Right.png'
+            this.lastDirection = 'Down'
+        }
+
+        draw(direction,x,y){
+            if(direction==='Idle'){
+                ctx.drawImage(this[`avatar${this.lastDirection}`], 0, 0, this.frameWidth, this.frameHeight, x, y, this.frameWidth, this.frameHeight) 
+                return 
+            }
+
+            this.lastDirection = direction
+            this.currentFrame++
+            let maxFrame = this.numColumns - 1
+            if (this.currentFrame > maxFrame){
+                this.currentFrame = 0
+            }
+            let column = this.currentFrame % this.numColumns;
+
+            ctx.drawImage(this[`avatar${direction}`], column * this.frameWidth, 0, this.frameWidth, this.frameHeight, x, y, this.frameWidth, this.frameHeight)
+        }
+    }
 
     class Character{
         constructor(){
-            this.y = 0
-            this.x = 0
+            this.y = 284
+            this.x = 384
             this.movingUp = false
             this.movingDown = false
             this.movingRight = false
             this.movingLeft = false
             this.speed = 4
             this.text = ''
-            this.prompt = incantation[0]
-            incantation.shift()
-            this.avatar = new Image()
-            this.height = 23
-            this.width = 14
+            this.prompt = 'start'
+            this.height = 32
+            this.width = 32
             this.recover = false
             this.lives = 3
-
-            this.avatar.onload = () => {
-              ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height);
-            }
-
-            this.avatar.src = 'Toad.png';
+            this.avatar = new Sprite()
 
             document.addEventListener('keydown',(event) => {
                 switch(event.key){
@@ -79,6 +115,26 @@ window.onload = () => {
                 }
             })
         }
+        direction(){
+            if(this.movingUp) return 'Up'
+            if(this.movingDown) return 'Down'
+            if(this.movingRight) return 'Right'
+            if(this.movingLeft) return 'Left'
+            return 'Idle'
+        }
+        draw(){
+            let frequency = 100
+            if (! this.recover || Math.floor(Date.now() / frequency) % 2) {  
+                this.avatar.draw(this.direction(),this.x,this.y)
+            }
+
+            const args = [
+                { text: `${this.text.toLowerCase()}` },
+                { text: `${this.prompt.charAt(0).toUpperCase() + this.prompt.slice(1)}`, fillStyle: 'blue' }
+            ]
+            
+            fillMixedText(ctx, args, this.x, this.y)
+        }
         isMovingDiagonal(){
             let directionCount = 0
             if(this.movingUp) directionCount++
@@ -90,37 +146,27 @@ window.onload = () => {
             }
             return false
         }
-        move(){//à changer pour prendre en compte la taille du char
+        move(){
             let currentSpeed = this.speed
             if(this.isMovingDiagonal())
                 currentSpeed = currentSpeed/2 * 1.41
     
-            if(this.movingLeft && this.x > 9)
-            this.x -= currentSpeed
-    
-            if(this.movingRight && this.x < canvasWidth - 22)
-            this.x += currentSpeed
-    
-            if(this.movingUp && this.y > 9)
-            this.y -= currentSpeed
-    
-            if(this.movingDown && this.y < canvasHeight - 33)
-            this.y += currentSpeed
-
-        }
-        draw(){
-            let frequency = 100
-            if (! this.recover || Math.floor(Date.now() / frequency) % 2) {  
-                ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height)
+            if(this.movingLeft){
+                this.x -= currentSpeed
+                if(this.x<0) this.x = 0
             }
-            //ctx.strokeText(this.prompt, this.x+(this.width/2) , this.y-5)
-
-            const args = [
-                { text: `${this.text}` },
-                { text: `${this.prompt}`, fillStyle: 'blue' }
-              ]
-              
-            fillMixedText(ctx, args, this.x, this.y-5)
+            if(this.movingRight){
+                this.x += currentSpeed
+                if(this.x > canvasWidth - this.width) this.x = canvasWidth - this.width
+            }
+            if(this.movingUp){
+                this.y -= currentSpeed
+                if(this.y < 0) this.y = 0
+            }
+            if(this.movingDown){
+                this.y += currentSpeed
+                if(this.y > canvasHeight - this.height) this.y = canvasHeight - this.height
+            }
         }
         hit(){
             if(!this.recover){
@@ -136,8 +182,7 @@ window.onload = () => {
             }
         }
         dead(){
-            alert("GAME OVER");
-            document.location.reload();
+            document.dispatchEvent(endGame)
         }
         type(char){
             if(char===this.prompt.charAt(0) || char.toUpperCase() === this.prompt.charAt(0)){
@@ -145,6 +190,12 @@ window.onload = () => {
                 this.prompt=this.prompt.substring(1)
             }
             if(this.prompt===''){
+                if(!gameStarted){
+                    document.dispatchEvent(startGame)
+                    gameStarted = true
+                }else{
+                this.wordTyped()
+                }
                 this.text=''
                 if(incantation.length===0){
                     this.cast()
@@ -155,7 +206,10 @@ window.onload = () => {
             }
         }
         cast(){
-            console.log('BRLRLRLA')
+            document.dispatchEvent(spellCast)
+        }
+        wordTyped(){
+            document.dispatchEvent(wordCast)
         }
     }
 
@@ -180,7 +234,7 @@ window.onload = () => {
             ctx.stroke()
         }
         collides(char){
-            if(char.x < this.x && this.x < char.x + char.width && char.y < this.y && this.y < char.y + char.height){
+            if(char.x+8 < this.x && this.x < char.x-8 + char.width && char.y+8 < this.y && this.y < char.y-8 + char.height){
                 char.hit()
                 return true
             }else{
@@ -191,22 +245,56 @@ window.onload = () => {
 
     class Foe{
         constructor(){
+            this.health = 100
+            this.numColumns = 8
+            this.currentFrame = 0 
             this.direction = 0
-            this.x = 350
-            this.y = 350
+            this.x = 50
+            this.y = 50
             this.speed = 2
             this.avatar = new Image()
-            this.height = 23
-            this.width = 14
+            this.height = 32
+            this.width = 32
             this.projectiles = []
-            this.avatar.onload = () => {
-              ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height)
+            this.avatar.onload = () => {}
+            this.avatar.src = 'glaringoverlord.png'
+
+            //document.addEventListener('spellcast', () => this.death())
+            document.addEventListener('wordcast', () => this.hit())
+        }
+
+        death(){
+            clearInterval(this.attackID)
+            this.speed = 0
+            document.dispatchEvent(endGame)
+        }
+
+        hit(){
+            this.health -= 20
+            if(this.health<=0){
+                this.health=0
+                this.death()
             }
-            this.avatar.src = 'Toad.png'
         }
 
         draw(){
-            ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height)
+            this.currentFrame++
+            let maxFrame = this.numColumns - 1
+            if (this.currentFrame > maxFrame){
+                this.currentFrame = 0
+            }
+            let column = this.currentFrame % this.numColumns;
+
+
+            let frequency = 100
+            if ( this.health || Math.floor(Date.now() / frequency) % 2) {  
+                ctx.drawImage(this.avatar, column * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+            }
+
+
+            ctx.fillStyle = "red";
+            ctx.fillRect(this.x, this.y-5, (this.width/100)*this.health, 3);
+        
             this.projectiles.forEach((item)=>{
                 item.draw()
             })
@@ -219,7 +307,7 @@ window.onload = () => {
                     this.projectiles.splice(index,1)
                 }
             })
-            if(char.x + char.width >= this.x && char.x <= this.x+this.width && char.y <= this.y + this.height && char.y + char.height >= this.y){
+            if(char.x + char.width -8  >= this.x && char.x +8 <= this.x+this.width && char.y +8<= this.y + this.height && char.y + char.height -8 >= this.y){
                 char.hit()
                 return true
             }else{
@@ -228,12 +316,18 @@ window.onload = () => {
         }
 
         attack(deg,speed){
-            this.projectiles.push(new Projectile(this.x,this.y,deg,speed))
+            this.projectiles.push(new Projectile(this.x+this.width/2,this.y+this.height/2,deg,speed))
         }
 
         circleAttack(nbProj, startAngle){
             for(let i = 1; i <= nbProj; i++){
                this.attack(Math.floor(startAngle+360*i/nbProj), 2)
+            }
+        }
+
+        shotgunAttack(nbProj, startAngle, spread){
+            for(let i = 0; i<nbProj; i++){
+                this.attack( startAngle + Math.floor(Math.random()*spread - spread/2) , Math.random()*4+1)
             }
         }
 
@@ -268,9 +362,26 @@ window.onload = () => {
                  this.direction = Math.floor(Math.random() * 360)
             }
         }
+
+        pointToAngle(x,y){
+            let deltaX = x - this.x
+            let deltaY = this.y - y
+            let result = Math.floor(Math.atan2(deltaY, deltaX)*(180/Math.PI))
+            return (result < 0) ? -result : (360 - result)
+        }
+
+        animate(atkFreq){
+            this.attackID = setInterval(()=>{
+                if(Math.random() < 0.5){
+                    this.circleAttack( Math.floor(Math.random() * 30), Math.floor(Math.random() * 360))
+                }else{
+                    this.shotgunAttack(10,Math.floor(Math.random()*360),20)
+                }
+            },atkFreq)
+        }
     }
 
-    const fillMixedText = (ctx, args, x, y) => {
+    function fillMixedText(ctx, args, x, y){//recup
         let defaultFillStyle = ctx.fillStyle
         let defaultFont = ctx.font
       
@@ -292,24 +403,51 @@ window.onload = () => {
 
     function render(){
         ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-        toad.draw()
-        badGuy.draw()
-        badGuy.move()
-        badGuy.moveProjectiles()
-        badGuy.collides(toad)
+        guy.move()
+        guy.draw()
+        if(badGuy){
+            badGuy.draw()
+            badGuy.move()
+            badGuy.moveProjectiles()
+            badGuy.collides(guy)
+        }
+        requestAnimationFrame(render)
     }
 
-    let toad = new Character()
-    let badGuy = new Foe()
-    badGuy.circleAttack(10,20)
+    let badGuy = false
+    let guy = new Character()
 
-    setInterval(function(){
-        toad.move()
-        render()
-    },20)
+    document.addEventListener('startgame', () =>{
+        badGuy = new Foe()
+        setTimeout(() => {
+            badGuy.animate(300)
+        }, 200);
+    })
+  
+    document.addEventListener('endgame', () =>{
+        while(document.querySelectorAll('#healthBar img').length<3){
+            document.querySelector('#healthBar').insertAdjacentHTML('afterbegin',`
+            <img src="health.png" alt="heart">
+            `)
+        }
+        guy.text=''
+        guy.prompt='start'
+        guy.lives=3
+        incantation=loremIpsum
+        setTimeout(() => {
+            badGuy = false
+            gameStarted = false
+        }, 4000);
+    })
 
-    setInterval(function(){
-        badGuy.circleAttack( Math.floor(Math.random() * 30), Math.floor(Math.random() * 360))
-    },1000)
+    requestAnimationFrame(render)
+
+    // setInterval(()=>{
+    //     badGuy.shotgunAttack(20,badGuy.pointToAngle(toad.x,toad.y),20)
+    // })
+    // render()
+
+   
+
 
   }
